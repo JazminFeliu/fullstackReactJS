c1790b40f0a566672e2d6e60c5e3419754be4894
JazminFeliu <jazminfeliu@gmail.com>
2022-02-24 17:48:14 -0300

    template strings con backtic

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const name = "Jazmin";
-alert(name);
+let fname = "Jaz";
+let lname = "F ";
+let age = prompt("Guess Jaz's age...");
 
+//old way
+// let result = fname + " " + lname + " " + age + " years old";
+// alert (result);
+
+//using template string
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
