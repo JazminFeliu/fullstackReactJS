8e6caefcab9b27d4429b6e795f9abeab7026edb9
JazminFeliu <jazminfeliu@gmail.com>
2022-03-07 13:09:07 -0300

    subo actualizacion arrow function

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-// let fname = "Jaz";
-// let lname = "F ";
-// let age = prompt("Guess Jaz's age...");
 
-//old way
-// let result = fname + " " + lname + " " + age + " years old";
-// alert (result);
+let nepal = {
+    //add property
+    mountains: ['Everestst','Fish Tail','Annapurna'],
 
-//using template string
-// let result = `${fname} ${lname} is ${age} years old`;
-// alert(result);
-
-// function welcome(user = "Mistery person", message = "Good day"){
-//     alert(`Hello ${user}, ${message}`);
-// }
-
-// welcome()
-
-// function gretting(message){
-//     return alert(`${message} everyone!`);    
-// }
-
-// let gretting = (message) => alert(`${message} everyone!`);
-
-// gretting('Good morning!');
-
-let createBlog = (title, body) => {
-    if(!title){
-        throw new Error( " A title is required");
+    //add method
+    printWithDash: function(){
+        setTimeout(() => console.log(this.mountains.join(" - ")),3000);
     }
-        
-    if (!body){
-        throw new Error(" Body cant be empty");
-    }
-
-    return alert(`${title} - ${body}`);
 };
 
-createBlog('Titulo del Blog', 'Blog body');
+nepal.printWithDash();
